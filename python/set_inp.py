18e360ff5c9ba843ec7949066c360e4a5bae4a20
hgs15624 <hgs15624@e099a375-04f9-0310-9d5f-a741eaff62e1>
2014-01-13 15:15:40 +0000

    Tune feedback: make it easier to change .INP
    Choice of local or remote PVs set to <ioc>:OFFSET1.INP.
    
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/ioc/CS/CS-DI-IOC-09@105423 e099a375-04f9-0310-9d5f-a741eaff62e1

--- python/set_inp.py
+++ python/set_inp.py
@@ -5,6 +5,8 @@ Simple script to set OFFSET1.INP for each magnet used in tune feedback.
 Each .INP is set to the local PV mirrored in our IOC.
 '''
 
+# unfortunately, need these imports so that we can load a function from
+# tunefb_server
 from pkg_resources import require
 require('cothread')
 require('scipy')
@@ -15,21 +17,28 @@ from cothread.catools import caput
 from tunefb_server import load_magnet_pvs, rename_pvs
 import sys, os
 
-
+# load file from same directory as the script
 PYDIR = os.path.dirname(os.path.realpath(__file__))
 PVS_FILE = os.path.join(PYDIR, 'TunePvs.txt')
-
-# provide an argument to enter test mode
-if len(sys.argv) > 1:
-    def caput(pvs, values):
-        for pv, val in zip(pvs, values):
-            print '%s   %s' % (pv, val)
-
 
 mag_pvs = load_magnet_pvs(PVS_FILE)
 local_pvs = rename_pvs(mag_pvs)
 
+if 'test' in sys.argv:
+    def caput(pvs, values):
+        for pv, val in zip(pvs, values):
+            print '%s   %s' % (pv, val)
+if 'redirect' in sys.argv:
+    # set INP to our PVs
+    links = [pv + ':I CPP MS' for pv in local_pvs]
+elif 'reset' in sys.argv:
+    # set INP to the remote PVs
+    links = [pv + ':LOFFSET1 CPP MS' for pv in mag_pvs]
+else:
+    print 'usage: '
+    print '    %s redirect|reset [test]' % sys.argv[0]
+    sys.exit()
+
 inps = [pv + ':OFFSET1.INP' for pv in mag_pvs]
-links = [pv + ':I CPP MS' for pv in local_pvs]
 
 caput(inps, links)
