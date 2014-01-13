231a288359a0963468b6a2def2aa0431cb4b70ba
hgs15624 <hgs15624@e099a375-04f9-0310-9d5f-a741eaff62e1>
2014-01-13 14:54:04 +0000

    Tune Feedback: Improved error handling and added docstrings
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/ioc/CS/CS-DI-IOC-09@105397 e099a375-04f9-0310-9d5f-a741eaff62e1

--- python/tunefb_server.py
+++ python/tunefb_server.py
@@ -7,10 +7,29 @@ import os, cothread, scipy, numpy, scipy.io
 from cothread.catools import caput, caget
 from softioc import builder
 
+
+# Errors
+NO_ERROR = 'No Errors'
+MAGNET_CURRENT_ERROR = 'Magnet current exceeds tolerance'
+TUNE_RANGE_ERROR = 'Tunes are outside allowable range'
+TUNE_DELTA_ERROR = 'Tunes are varying to rapidly'
+LOW_CURRENT_ERROR = 'Current is to low'
+
+
 class tunefb_server(object):
 
+    """
+    Server for tune feedback. Creates PVs, and monitors and then
+    corrects tune towards a setpoint.
+    """
+
     def __init__(self, mode):
+        """Initalise values."""
+
+        # Initial values for PVs
         self.power = 1
+        self.afrac = 0.2
+        self.last_error = NO_ERROR
 
         # Tune data
         self.tune_pvs = [
@@ -18,8 +37,10 @@ class tunefb_server(object):
                 'SR21C-DI-TMBF-02:TUNE:TUNE']
         self.golden_tunes = numpy.array([0.201, 0.371])
         self.max_delta_tunes = numpy.array([0.1, 0.1])
-        self.max_tune_variance = numpy.array([0.1, 0.1])
-        self.delta_tunes = self.golden_tunes - numpy.array(caget(self.tune_pvs))
+        self.max_tunes = self.golden_tunes + 0.1
+        self.min_tunes = self.golden_tunes - 0.1
+        self.delta_tunes = (self.golden_tunes -
+                numpy.array(caget(self.tune_pvs)))
 
         # Current checking values
         self.current_pv = 'SR-DI-DCCT-01:SIGNAL'
@@ -31,12 +52,18 @@ class tunefb_server(object):
         # Load data from files (and on ringmode change)
         self.dataroot = '/home/uxj42447/software/fastfeedback.data'
         self.set_datadir('SRI0913')
-        mode.add_listener(self.set_datadir)
+        if self.set_datadir not in mode.listeners:
+            mode.add_listener(self.set_datadir)
 
         # Initalise EPICS records
+        self.afrac_pv = None
+        self.power_pv = None
+        self.error_pv = None
+        self.reset_pv = None
         self.records()
 
     def set_datadir(self, datadir):
+        """Load required data from files in datadir."""
         dir = os.path.join(self.dataroot, datadir)
 
         # Magnet pv names
@@ -57,32 +84,46 @@ class tunefb_server(object):
             self.irms.append((numpy.linalg.pinv(raw_rm[0][0][0])))
 
     def init(self):
+        """Spawn a new thread to run the main ioc loop."""
         cothread.Spawn(self.tick)
 
     def tick(self):
+        """Top level loop in the ioc, if it terminates then
+        a restart of the ioc is required. Therefore, it is appropraite
+        to catch all exceptions.
+        """
         while True:
-            if self.power:
-                self.do_correction()
-            cothread.Sleep(1.0)
+            try:
+                if self.power:
+                    self.do_correction()
+                cothread.Sleep(1.0)
+            except Exception, e:
+                print 'Error:', e
 
     def check_current(self):
+        """Check if current is greater than a minium current."""
         if caget(self.current_pv) < self.min_current:
-            raise Exception('Current to low')
+            raise Exception(LOW_CURRENT_ERROR)
 
-    def is_injection_occuring(self):
+    def is_injection_occurring(self):
+        """Check if topup injection is occurring."""
         if caget(self.injection_pv) == 0:
             return True
         return False
 
     def refresh_delta_tunes(self):
-        delta_tunes = self.golden_tunes - numpy.array(caget(self.tune_pvs))
-        if (abs(delta_tunes - self.delta_tunes) > self.max_delta_tunes).any():
-            raise Exception('Tunes are varying to fast')
-        self.delta_tunes = delta_tunes
-        if (abs(delta_tunes) > self.max_tune_variance).any():
-            raise Exception('Tunes outside of permitted range')
+        """Update values for self.delta_tunes."""
+        tunes = numpy.array(caget(self.tune_pvs))
+        self.delta_tunes = self.golden_tunes - tunes
+        if (tunes > self.max_tunes).any():
+            raise Exception(TUNE_RANGE_ERROR)
+        if (tunes < self.min_tunes).any():
+            raise Exception(TUNE_RANGE_ERROR)
+        if (abs(self.delta_tunes) > self.max_delta_tunes).any():
+            raise Exception(TUNE_DELTA_ERROR)
 
     def apply_correction(self, deltas):
+        """Put delta correction to magnets."""
         mag_vals = [numpy.array(caget(pvs)) for pvs in self.mag_pvs]
         for i, delta in enumerate(deltas):
             mag_vals[i] += delta
@@ -90,26 +131,96 @@ class tunefb_server(object):
                 (x[0] > x[1][1]).any()
                 or (x[0] < x[1][0]).any()
                 for x in zip(mag_vals, self.mag_limits)]).any():
-            raise Exception('Magnet current values exceed tolerances')
-        [caput(x[0], x[1]) for x in zip(self.mag_pvs, mag_vals)]
+            raise Exception(MAGNET_CURRENT_ERROR)
+        #[caput(x[0], x[1]) for x in zip(self.mag_pvs, mag_vals)]
 
     def do_correction(self):
+        """Calculate and then apply a correction, subject to checks."""
         try:
             self.check_current()
-            if not self.is_injection_occuring():
+            if not self.is_injection_occurring():
                 self.refresh_delta_tunes()
                 deltas = [
-                    numpy.dot(irm, self.delta_tunes)
-                    for irm in self.irms]
+                    numpy.dot(irm, self.delta_tunes) for irm in self.irms]
+                deltas = [delta*self.afrac for delta in deltas]
                 self.apply_correction(deltas)
         except Exception, e:
-            self.set_power(0)
-            print 'Error:', e
+            self.power_pv.set(0)
+            self.error_pv.set(e.__str__())
+            print 'Error', e
 
-    def set_power(self, power):
-        self.power = power
+    def reset(self, dummy):
+        """Reset the error pv."""
+        self.error_pv.set(NO_ERROR)
+        self.reset_pv.set(0)
+        print 'reset called', dummy
+
+    def set_power(self, value):
+        self.power = value
+
+    def set_afrac(self, value):
+        self.afrac = value
+
+    def set_tune_h(self, value):
+        self.golden_tunes[0] = value
+
+    def set_tune_v(self, value):
+        self.golden_tunes[1] = value
+
+    def set_max_h_tune(self, value):
+        self.max_tunes[0] = value
+
+    def set_max_v_tune(self, value):
+        self.max_tunes[1] = value
+
+    def set_min_h_tune(self, value):
+        self.min_tunes[0] = value
+
+    def set_min_v_tune(self, value):
+        self.min_tunes[1] = value
+
+    def set_min_v_tune(self, value):
+        self.min_tunes[1] = value
+
+    def set_delta_h_tune(self, value):
+        self.max_delta_tunes[0] = value
+
+    def set_delta_v_tune(self, value):
+        self.max_delta_tunes[1] = value
 
     def records(self):
+        """Setup iocbuilder to create required records."""
         builder.SetDeviceName("SR-CS-TCFB-01")
-        self.power_pv = builder.mbbOut('ONOFF', ("OFF", 0), ("ON", 1),
-                initial_value = self.power, on_update = self.set_power)
+        self.afrac_pv = builder.aOut(
+                'AFRAC', initial_value=0.2, on_update=self.set_afrac,PREC=4)
+        self.power_pv = builder.mbbOut(
+                'ONOFF', ("OFF", 0), ("ON", 1),
+                initial_value=self.power, on_update=self.set_power)
+        self.error_pv = builder.stringOut(
+                'ERROR', initial_value=NO_ERROR)
+        self.reset_pv = builder.aOut(
+                'RESET', initial_value=0, on_update=self.reset)
+        builder.aOut(
+                'TUNE:H', initial_value=self.golden_tunes[0],
+                on_update=self.set_tune_h, PREC=4)
+        builder.aOut(
+                'TUNE:V', initial_value=self.golden_tunes[1],
+                on_update=self.set_tune_v, PREC=4)
+        builder.aOut(
+                'TUNE:HMAX', initial_value=self.max_tunes[0],
+                on_update=self.set_max_h_tune, PREC=4)
+        builder.aOut(
+                'TUNE:VMAX', initial_value=self.max_tunes[1],
+                on_update=self.set_min_v_tune, PREC=4)
+        builder.aOut(
+                'TUNE:HMIN', initial_value=self.min_tunes[0],
+                on_update=self.set_min_v_tune, PREC=4)
+        builder.aOut(
+                'TUNE:VMIN', initial_value=self.min_tunes[1],
+                on_update=self.set_min_v_tune, PREC=4)
+        builder.aOut(
+                'TUNE:HDELTA', initial_value=self.max_delta_tunes[0],
+                on_update=self.set_delta_h_tune, PREC=4)
+        builder.aOut(
+                'TUNE:VDELTA', initial_value=self.max_delta_tunes[1],
+                on_update=self.set_delta_v_tune, PREC=4)
