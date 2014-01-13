faf0a2ffd44fb0db6688f826b9c6166ec0bfb14e
hgs15624 <hgs15624@e099a375-04f9-0310-9d5f-a741eaff62e1>
2014-01-13 15:15:33 +0000

    Tune feedback: simple script to set magnet inputs
    Set OFFSET1.INP on the magnet power supplies to the appropriate link
    for using our mirror PVs.
    
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/ioc/CS/CS-DI-IOC-09@105413 e099a375-04f9-0310-9d5f-a741eaff62e1

---
+++ python/set_inp.py
@@ -0,0 +1,25 @@
+#!/dls_sw/work/R3.14.11/support/pythonSoftIoc/pythonIoc
+
+from pkg_resources import require
+require('cothread')
+require('scipy')
+require('numpy')
+require('iocbuilder')
+
+#from cothread.catools import caput
+from tunefb_server import load_magnet_pvs, rename_pvs
+
+PVS_FILE = '/home/uxj42447/software/fastfeedback.data/SRI0913/TunePvs.mat'
+
+def caput(pvs, values):
+    for pv, val in zip(pvs, values):
+        print '%s   %s' % (pv, val)
+
+
+mag_pvs = load_magnet_pvs(PVS_FILE)
+local_pvs = rename_pvs(mag_pvs)
+
+inps = [pv + ':OFFSET1.INP' for pv in mag_pvs]
+links = [pv + ':I CPP MS' for pv in local_pvs]
+
+caput(inps, links)
