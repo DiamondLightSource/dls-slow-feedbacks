f1827e50970c8e572b6d27d1884caa28f4570c05
hgs15624 <hgs15624@e099a375-04f9-0310-9d5f-a741eaff62e1>
2014-07-07 14:52:08 +0000

    Tune feedback: add a PV showing max offset current
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/ioc/CS/CS-DI-IOC-09@116522 e099a375-04f9-0310-9d5f-a741eaff62e1

--- python/tunefb_server.py
+++ python/tunefb_server.py
@@ -185,6 +185,7 @@ class TunefbServer(object):
 
         # Initalise EPICS records
         self.records()
+        self.max_i_pv.set(max(abs(i) for i in self.startup_currents))
 
     def set_datadir(self, datadir):
         '''Load required data from files in datadir.'''
@@ -291,6 +292,7 @@ class TunefbServer(object):
         return deltas
 
     def check_mag_limits(self, currents):
+        self.max_i_pv.set(max(abs(i) for i in currents))
         if any(currents < self.mag_limits[0]):
             raise TunefbError(Status.MAGNET_CURRENT)
         if any(currents > self.mag_limits[1]):
@@ -310,6 +312,7 @@ class TunefbServer(object):
             log.warn(OFFSET_CURRENT_CHANGED)
 
         self.integrated_current = fetched_current + deltas
+        self.max_i_pv.set(max(abs(self.integrated_current)))
         for pv, current in zip(self.mirror_pvs, self.integrated_current):
             pv.set(current)
         log.info(
@@ -465,6 +468,8 @@ class TunefbServer(object):
         builder.aOut(
                 'ILIM', initial_value=self.max_current_range,
                 on_update=self.set_max_current_range, PREC=4)
+        self.max_i_pv = builder.aOut(
+                'IMAX', initial_value=0.0, PREC=4)
         builder.aOut(
                 'BEAMMIN', initial_value=self.min_beam_current,
                 on_update=self.set_min_beam_current, PREC=4)
