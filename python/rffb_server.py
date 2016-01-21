7446a4e2191c6d57e16f4391258c843388c24358
Matthew Furseman <matthew.furseman@diamond.ac.uk>
2016-01-21 13:01:20 +0000

    Change list of ringmode to only contain VMX
    The new hardware will be incompatible with the old modes. Showing them
    in the GUI could lead to confusion and they won't work as execpted.

--- python/rffb_server.py
+++ python/rffb_server.py
@@ -14,9 +14,7 @@ import rffb_calc
 
 
 class ringmode(object):
-    ring_modes = [
-        "SR", "SRI13", "SRI0913", "SRLE3ps", "SRLEm3ps",
-        "SRLETHz", "SRI0913_MOGA", "SRI21", "VMX"]
+    ring_modes = ["VMX"]
 
     def __init__(self):
         self.records()
