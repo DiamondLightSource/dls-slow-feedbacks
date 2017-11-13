4ea786a951c826f6b72669bb3809752799b9c6c2
Will Rogers <wahrogers@gmail.com>
2017-11-13 11:17:00 +0000

    Adapt RingMode object to work with pytac lattice.

--- python/mode.py
+++ python/mode.py
@@ -1,4 +1,7 @@
+import sys
+sys.path.append('/home/hgs15624/code/python/pytac')
 from softioc import builder
+from pytac import load_csv
 
 
 RING_MODES = ["SR", "SRI13", "SRI0913", "SRLE3ps", "SRLEm3ps",
@@ -12,11 +15,17 @@ DEFAULT_RING_MODE = 'VMX'
 DATAROOT = "/dls_sw/work/common/matlab/mml/machine/diamondopsdata"
 
 
+def load_pml_lattice(ringmode):
+    lattice = load_csv.load(ringmode)
+    return lattice
+
+
 class RingMode(object):
 
     def __init__(self):
         self.records()
         self.listeners = []
+        self.name = DEFAULT_RING_MODE
 
     def records(self):
         builder.SetDeviceName('SR-CS-RING-01')
@@ -24,11 +33,13 @@ class RingMode(object):
                                    *zip(RING_MODES, range(len(RING_MODES))))
 
     def init(self):
-        self.mode.set(RING_MODES.index(DEFAULT_RING_MODE))
+        self.mode.set(RING_MODES.index(self.name))
 
     def set_mode(self, mode):
+        self.name = RING_MODES[mode]
+        lattice = load_pml_lattice(self.name)
         for l in self.listeners:
-            l(RING_MODES[mode])
+            l(lattice)
 
     def add_listener(self, listener):
         self.listeners.append(listener)
