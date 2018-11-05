792f583121817d34aea7aceae1450c22c4b0441d
Andrew Wilson <andrew.wilson@diamond.ac.uk>
2018-11-05 12:59:37 +0000

    Remove non-working unit tests. Make names more descriptive.

--- tests/test_rffb.py
+++ tests/test_rffb.py
@@ -68,7 +68,7 @@ def lookup_caget_value(pv_name, *args, **kwargs):
 
 
 class TestMockPv(unittest.TestCase):
-    def test_creating(self):
+    def test_creating_MockPV(self):
         name = "MOCK-PV-01"
         value = 1.234
         pv = MockPV(name, value)
@@ -79,7 +79,7 @@ class TestMockPv(unittest.TestCase):
         self.assertEqual(got.severity, constants.SEVR_NO_ALARM)
         self.assertEqual(got.ok, True)
 
-    def test_creating_with_severity(self):
+    def test_creating_MockPV_with_severity(self):
         name = "MOCK-PV-02"
         value = 2.345
         pv = MockPV(name, value, severity=constants.SEVR_INVALID)
@@ -93,14 +93,14 @@ class TestMockPv(unittest.TestCase):
 
 class PVWithValidityTests(unittest.TestCase):
     @patch("cothread.catools.caget", side_effect=lookup_caget_value)
-    def test_creating(self, mock_caget):
+    def test_creating_PVWithValidity(self, mock_caget):
         pv = rffb_server.PVWithValidity("MOCK-PV-03")
         value_should_be = caget_value_dict["MOCK-PV-03"].get()
         self.assertEqual(pv.get(), value_should_be)
         self.assertEqual(pv.severity, value_should_be.severity)
 
     @patch("cothread.catools.caget", side_effect=lookup_caget_value)
-    def test_threshold(self, mock_caget):
+    def test_invalid_cagets_over_threshold_return_not_healthy(self, mock_caget):
         # Create a mock PV
         pv = rffb_server.PVWithValidity("MOCK-PV-04")
         value_should_be = caget_value_dict["MOCK-PV-04"].get()
@@ -121,7 +121,7 @@ class PVWithValidityTests(unittest.TestCase):
                 self.assertFalse(pv.healthy())
 
     @patch("cothread.catools.caget", side_effect=lookup_caget_value)
-    def test_almost_threshold(self, mock_caget):
+    def test_resetting_severity_makes_PV_healthy(self, mock_caget):
         # Create a mock PV
         pv = rffb_server.PVWithValidity("MOCK-PV-05")
         value_should_be = caget_value_dict["MOCK-PV-05"].get()
@@ -145,13 +145,12 @@ class PVWithValidityTests(unittest.TestCase):
             self.assertEqual(pv.get(), value_should_be)
             self.assertEqual(pv.severity, value_should_be.severity)
 
-            if i < pv.ALLOWED_INVALID_CAGETS:
-                self.assertTrue(pv.healthy())
+            self.assertTrue(pv.healthy())
 
 
 class RffbTests(unittest.TestCase):
 
-    def test_rf_near_setpoint_bad(self):
+    def test_rf_near_setpoint_with_bad_values_returns_false(self):
         """Frequency difference > 100 Hz"""
         present_rf_freq = 499682023
         rf_setpoint = 499681023
@@ -160,72 +159,13 @@ class RffbTests(unittest.TestCase):
                 present_rf_freq,
                 rf_setpoint))
 
-    def test_rf_near_setpoint_good(self):
+    def test_rf_near_setpoint_with_good_values_returns_true(self):
         """Frequencey difference < 100 Hz"""
         present_rf_freq = 499682023
         rf_setpoint = 499682023
         self.assertTrue(rffb_server.RffbServer.rf_near_setpoint(
             present_rf_freq,
             rf_setpoint))
-
-
-class RffbServerFactory(object):
-    """Class to hand out fresh copies of the RffbServer object
-    In an attempt to get around iocbuilder's restriction on
-    defining record names only once
-
-    Edit: not as clever as I thought, the copy contains references
-    """
-    def __init__(self):
-        ring_mode = mode.RingMode()
-        self.rffb = rffb_server.RffbServer(ring_mode)
-
-    def get_object(self):
-        return copy.copy(self.rffb)
-
-# Done at module level so is only instantiated once
-rffb_server_factory = RffbServerFactory()
-
-class TestRffbServerFactory(unittest.TestCase):
-    @patch("cothread.catools.caget", side_effect=lookup_caget_value)
-    def test_modifying_one_copy_doesnt_modify_other(self, mock_caget):
-        rffb1 = rffb_server_factory.get_object()
-        rffb2 = rffb_server_factory.get_object()
-
-        rffb1.power_pv.set(1)
-        rffb2.power_pv.set(0)
-        self.assertNotEquals(rffb2.power_pv.get(), rffb1.power_pv.get())
-
-class IntegrationTests(unittest.TestCase):
-
-    @patch("cothread.catools.caget", side_effect=lookup_caget_value)
-    def setUp(self, mock_caget):
-        self.rffb = rffb_server_factory.get_object()
-
-    def tearDown(self):
-        self.rffb = None
-
-    def test_records_created(self):
-        self.assertIsInstance(self.rffb.rf_freq_set_pv, rffb_server.PVWithValidity)
-        self.assertIsNotNone(self.rffb.power_pv)
-
-    def test_caget_of_nonexistent_pv_raises_exception(self):
-        #caget_value_dict["MOCK-PV-05"] = MockPV()
-        with self.assertRaises(cothread.catools.ca_nothing):
-            self.rffb.feedback()
-
-    def test_caget_of_nonexistent_pv_raises_exception_again(self):
-        # caget_value_dict["MOCK-PV-05"] = MockPV()
-        with self.assertRaises(cothread.catools.ca_nothing):
-            self.rffb.feedback()
-
-"""
-    def test_rf_frequency_and_setpoint_differ(self):
-        present_rf_freq = 499682023
-        rf_setpoint = 499681023
-        caget_value_dict["MOCK-PV-05"] = MockPV()
-
-"""
 
 if __name__ == "__main__":
     unittest.main()
