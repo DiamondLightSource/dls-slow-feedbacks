64918e7db9359f28a871aed99dabcd7c3e92c259
Martin Gaughran <martin.gaughran@diamond.ac.uk>
2021-08-18 23:12:58 +0100

    Replace sofb-gui script with Python entry point

---
+++ dls_slow_feedbacks/start_edm.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import sys
+from pathlib import Path
+
+
+def run_edm_gui(edl_file):
+    script_dir = Path(os.path.dirname(os.path.realpath(__file__)))
+    os.chdir(script_dir / "opi")
+
+    if len(sys.argv) > 1:
+        os.environ["EPICS_CA_SERVER_PORT"] = 6064
+
+    subprocess.run(["edm", "-x", "-eolc", edl_file])
+
+
+def sofb_gui():
+    run_edm_gui("rffb.edl")
