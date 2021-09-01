80111d4027dff826bca85539a755d4b921aef81e
Martin Gaughran <martin.gaughran@diamond.ac.uk>
2021-09-01 18:13:38 +0100

    Fix version search to ignore subdirectories
    This fixes a bug where it finds the _version_git.py file in modules
    installed in the lightweight venv that is produced during release.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ import sys
 from setuptools import setup
 
 # Place the directory containing _version_git on the path
-for path, _, filenames in os.walk(os.path.dirname(os.path.abspath(__file__))):
-    if "_version_git.py" in filenames:
-        sys.path.append(path)
-        break
+TOP = os.path.dirname(os.path.abspath(__file__))
+for d in os.listdir(TOP):
+    if os.path.exists(os.path.join(TOP, d, "_version_git.py")):
+        sys.path.append(os.path.join(TOP, d))
 
 from _version_git import __version__, get_cmdclass  # noqa
 
