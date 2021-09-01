86251bbdafc4e49eb1174f5f849002226914ef06
Martin Gaughran <martin.gaughran@diamond.ac.uk>
2021-09-01 18:16:04 +0100

    Add Python script to replace runcss.sh

---
+++ dls_slow_feedbacks/start_css.py
@@ -0,0 +1,89 @@
+import argparse
+import os
+import subprocess
+import sys
+from pathlib import Path
+
+from dls_slow_feedbacks._version_git import __version__
+
+
+def get_opi_dir():
+    script_dir = Path(os.path.dirname(os.path.realpath(__file__)))
+    return script_dir / "opi"
+
+
+def get_bin_dir():
+    bin_folder = os.path.dirname(os.path.realpath(sys.argv[0]))
+    return bin_folder
+
+
+def get_css():
+    output = subprocess.run(
+        ["configure-ioc", "s", "-p", "CSS-gui"], capture_output=True
+    )
+    return output.stdout.strip()
+
+
+def generate_css_args(launch_opi, links, macros, extra_args):
+    args = ["-o", launch_opi, "-s", "-l", links]
+    if macros:
+        args += ["-m", macros]
+    args += extra_args
+
+    return args
+
+
+def run_css_gui(launch_opi, links, macros, extra_args):
+    css_prog = get_css()
+
+    command = [css_prog]
+    command += generate_css_args(launch_opi, links, macros, extra_args)
+    subprocess.run(command)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Run CSS OPI files. Additional arguments are passed to css.sh.",
+        # Need ellipsis at end of standard usage message to indicate args that get
+        # passed to css.sh
+        usage="start-css [-h] [-m MACROS] opi_file ...",
+    )
+
+    parser.add_argument(
+        "opi_file",
+        help="opi_file path should be relative to opi/ directory",
+    )
+
+    # Specify macros (even though all extra args are forwarded) to improve help message
+    parser.add_argument(
+        "-m", dest="macros", help="CSS macros in format: macro1=value1,macro2=value2"
+    )
+
+    return parser.parse_known_args()
+
+
+def validate_opi_file(opi_path):
+    opi_valid = opi_path.is_file() and opi_path.suffix == ".opi"
+    if not opi_valid:
+        raise OSError(f"The opi path does not link to an opi file:\n{opi_path}")
+
+
+def main():
+    parsed_args, unparsed_args = parse_arguments()
+
+    opi_file = parsed_args.opi_file
+
+    module = "dls_slow_feedbacks"
+    project = f"{module}_{__version__}"
+    launch_opi = f"/{project}/{module}/{opi_file}"
+
+    opi_dir = get_opi_dir()
+    links = f"{opi_dir}={project}/{module}"
+
+    validate_opi_file(opi_dir / opi_file)
+
+    bin_dir = get_bin_dir()
+    os.environ["PATH"] = bin_dir + os.pathsep + os.environ["PATH"]
+
+    os.chdir(opi_dir)
+    run_css_gui(launch_opi, links, parsed_args.macros, unparsed_args)
