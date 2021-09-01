fa625b510246db7ce2db710d148be7d9c9fd85da
Martin Gaughran <martin.gaughran@diamond.ac.uk>
2021-09-01 18:04:14 +0100

    Rename bin_folder to bin_dir in start_edm for consistency

--- dls_slow_feedbacks/start_edm.py
+++ dls_slow_feedbacks/start_edm.py
@@ -26,6 +26,6 @@ def vefb_gui():
 
 
 def tunefb_gui():
-    bin_folder = os.path.dirname(os.path.realpath(sys.argv[0]))
-    macros = f'"bin_path={bin_folder}"'
+    bin_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+    macros = f'"bin_dir={bin_dir}"'
     run_edm_gui("tfb.edl", macros)
