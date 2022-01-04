9f4125ddd37c9e4d5a118b368fb638d6ef052f88
Andy Wilson <andrew.wilson@diamond.ac.uk>
2022-01-04 14:12:23 +0000

    Add method to get BPM and Corrector disabled PVs
    For use in generating a burt request file for these PVs

---
+++ dls_slow_feedbacks/util.py
@@ -0,0 +1,19 @@
+from dls_slow_feedbacks import waveforms, mode
+
+
+def get_bpm_and_corrector_feedback_disabled_pvs() -> list:
+    """Return a list of the PVs created for 'BPMs and Correctors enabled' in feedbacks
+
+    This can be used to generate a Burt request file for these PVs, should this be
+    required when the lattice changes.
+    """
+    pvs = []
+    ring_mode = mode.RingMode()
+    waveform_obj = waveforms.WaveformsServer(ring_mode)
+    for family in waveform_obj.records.values():
+        for feedback_speed in family.values():
+            for plane in feedback_speed:
+                for record in plane:
+                    pvs.append(record.name)
+
+    return pvs
